72d463c32a1521003094af5bc57092653b625754
TinoyAlvX111 <alvintinoy125@gmail.com>
2026-03-21 19:21:38 +0800

    backend/enrollment_system/settings.py

--- backend/enrollment_system/settings.py
+++ backend/enrollment_system/settings.py
@@ -56,12 +56,8 @@ WSGI_APPLICATION = 'enrollment_system.wsgi.application'
 # Database Configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'enrollment_db',
-        'USER': 'postgres',
-        'PASSWORD': 'kent2005', 
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
